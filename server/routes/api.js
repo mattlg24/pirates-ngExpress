de558e9efc17ba3cdd37a9a157f713ae58ac215f
Matt Gardner <matthewlgardner@gmail.com>
2016-11-01 11:33:23 -0600

    render all pirates on page

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,16 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    knex('pirates')
+        .then(pirates => {
+            console.log('all the pirates are', pirates);
+            res.json(pirates)
+        })
 });
 
 module.exports = router;
